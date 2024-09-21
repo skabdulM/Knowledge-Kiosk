31fc05cb4e997076ae0be0198ea82886ced0c26c
nabilaqureshi23 <127545408+nabilaqureshi23@users.noreply.github.com>
2024-09-21 13:14:48 +0530

    Update Teacher.tsx

--- frontend/src/pages/Teacher.tsx
+++ frontend/src/pages/Teacher.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import assign from "../assets/assign.png";
+import class_schedule from "../assets/class_schedule.jpg";
+import exam from "../assets/exam.jpg";
 import {
   IonPage,
   IonContent,
@@ -6,30 +9,152 @@ import {
   IonHeader,
   IonTitle,
   IonToolbar,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonGrid,
+  IonRow,
+  IonMenu,
+  IonSplitPane,
 } from "@ionic/react";
+
 import Navbar from "../components/Navbar"; // Assuming Navbar is a separate component
-import "./Home.css"; // External CSS for styling
-import {
-  Parallax,
-  ParallaxBanner,
-  ParallaxBannerLayer,
-  ParallaxProvider,
-} from "react-scroll-parallax";
-import { AdvancedBannerTop } from "../components/AdvancedPrallax";
+import "./Teacher.css";
 
 const Teacher: React.FC = () => {
-  const [scrollY, setScrollY] = useState(0);
-
-  const handleScroll = (event: CustomEvent) => {
-    const scrollTop = event.detail.scrollTop;
-    setScrollY(scrollTop);
-  };
+  const cards = [
+    {
+      name: "Add Student",
+      description: "card desc",
+      img: "https://cdn2.iconfinder.com/data/icons/user-23/512/User_Preppy_4.png",
+    },
+    {
+      name: "Assignment ",
+      description: "card desc",
+      img: "frontendsrcassign.png",
+    },
+    {
+      name: "card 1",
+      description: "card desc",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwkShLbqYSTnT8SemNHJWZzuNuXdrSupU-N0rZpJ9PvbfhHL8SFZzQ0gVAL0XjZUgZSnI&usqp=CAU",
+    },
+    {
+      name: "card 1",
+      description: "card desc",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwkShLbqYSTnT8SemNHJWZzuNuXdrSupU-N0rZpJ9PvbfhHL8SFZzQ0gVAL0XjZUgZSnI&usqp=CAU",
+    },
+    {
+      name: "card 1",
+      description: "card desc",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwkShLbqYSTnT8SemNHJWZzuNuXdrSupU-N0rZpJ9PvbfhHL8SFZzQ0gVAL0XjZUgZSnI&usqp=CAU",
+    },
+    {
+      name: "fee recipt",
+      description: "card desc",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwkShLbqYSTnT8SemNHJWZzuNuXdrSupU-N0rZpJ9PvbfhHL8SFZzQ0gVAL0XjZUgZSnI&usqp=CAU",
+    },
+    {
+      name: "card 1",
+      description: "card desc",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwkShLbqYSTnT8SemNHJWZzuNuXdrSupU-N0rZpJ9PvbfhHL8SFZzQ0gVAL0XjZUgZSnI&usqp=CAU",
+    },
+    {
+      name: "events  1",
+      description: "card desc",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwkShLbqYSTnT8SemNHJWZzuNuXdrSupU-N0rZpJ9PvbfhHL8SFZzQ0gVAL0XjZUgZSnI&usqp=CAU",
+    },
+  ];
 
   return (
     <IonPage>
       <IonContent>
         <Navbar />
-        aascdsa
+
+        <div>
+          <IonGrid>
+            <IonRow className="ion-justify-content-center">
+              {/* Add Student Card */}
+              <IonCol size="5" size-md="2">
+                <IonCard className="dashboard-card">
+                  <img
+                    src="https://cdn2.iconfinder.com/data/icons/user-23/512/User_Preppy_4.png"
+                    alt="Add Student"
+                    className="card-image"
+                  />
+                  <IonCardContent className="card-title">
+                    Add Student
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+              <IonCol size="5" size-md="2">
+                <IonCard className="dashboard-card">
+                  <img
+                    src={assign}
+                    alt="Assignment Management"
+                    className="card-image"
+                  />
+                  <IonCardContent className="card-title">
+                    Assignment Management
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+              <IonCol size="5" size-md="2">
+                <IonCard className="dashboard-card">
+                  <img
+                    src={class_schedule}
+                    alt="Class Schedule"
+                    className="card-image"
+                  />
+                  <IonCardContent className="card-title">
+                    Class Schedule
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+              <IonCol size="5" size-md="2">
+                <IonCard className="dashboard-card">
+                  <img
+                    src={exam}
+                    alt="Exam Management"
+                    className="card-image"
+                  />
+                  <IonCardContent className="card-title">
+                    Exam Management
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-justify-content-center ion-margin-top">
+              <IonCol size="5" size-md="2">
+                <IonCard className="dashboard-card">
+                  <img
+                    src="https://i.pinimg.com/736x/19/fb/01/19fb0153d29d0517821d03112098ec1f.jpg"
+                    alt="Class Schedule"
+                    className="card-image"
+                  />
+                  <IonCardContent className="card-title">
+                    Generate Class Report
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+
+              <IonCol size="5" size-md="2">
+                <IonCard className="dashboard-card">
+                  <img
+                    src="https://i.pinimg.com/474x/0d/5d/ea/0d5dea4c3d32dc65598850d3e4bdd625.jpg"
+                    alt="Class Schedule"
+                    className="card-image"
+                  />
+                  <IonCardContent className="card-title">
+                    Communication Tools
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
       </IonContent>
     </IonPage>
   );
